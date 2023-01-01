d98d9156ca18cdd8bbe886231eab35015a20de2b
StevenJ <jshl8612@gmail.com>
2023-01-01 23:23:39 +0900

    feat: Add API Tests

---
+++ kidsnote/kidsnoteTests/APITests.swift
@@ -0,0 +1,31 @@
+//
+//  APITests.swift
+//  kidsnoteTests
+//
+//  Created by Steven Jiang on 2022/12/30.
+//
+
+import XCTest
+@testable import kidsnote
+
+final class APITests: XCTestCase {
+
+    
+    let url = URL(string: "https://www.googleapis.com/books/v1/volumes?q=law")
+    
+    func testAPISuccess() {
+        let exp = XCTestExpectation(description: "API Request Succeed")
+        
+        Task {
+            do {
+                let result = try await API().request(type: SearchResult.self, url: url!)
+                XCTAssertNotNil(result)
+                exp.fulfill()
+            }
+            catch {
+                exp.fulfill()
+            }
+        }
+        wait(for: [exp], timeout: 5)
+    }
+}
