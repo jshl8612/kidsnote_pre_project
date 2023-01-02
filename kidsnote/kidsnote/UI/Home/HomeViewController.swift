0658688714466639a53384f8402df6ebd288f7e3
StevenJ <jshl8612@gmail.com>
2023-01-02 13:41:45 +0900

    fix: fix searchBar not work issue

--- kidsnote/kidsnote/UI/Home/HomeViewController.swift
+++ kidsnote/kidsnote/UI/Home/HomeViewController.swift
@@ -18,7 +18,7 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.navigationItem.titleView = homeView.searchBar
     }
     
 
